58c379c77df768a00a608fc086700a8bd3382af7
Damion Wiltshire <drw.5452@gmail.com>
2020-09-22 06:35:06 -0700

    specifying multiple origins correctly

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'https://protected-spire-28392.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
