50cf07bbbe0daece76f68195eaa01d1521af84f4
Damion Wiltshire <drw.5452@gmail.com>
2020-09-22 06:53:15 -0700

    fix CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000/', 'https://protected-spire-28392.herokuapp.com/'
+    origins 'http://localhost:3000/'
 
     resource '*',
       headers: :any,
