ff140894dfc0c579164731ee80ef650730a48c88
Damion Wiltshire <drw.5452@gmail.com>
2020-09-22 10:36:45 -0700

    add routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Tests connection between backend and UI
   get "/" => "users#test"
+  
+  # Call create method on SessionsController class
+  post '/login'  => 'sessions#create'
+
+  # Call destroy method on SessionsController class
+  delete '/logout' => 'sessions#destroy'
+ 
+  # Call profile method on Users class class
+  get '/profile' => 'users#profile'
+  
+  resources :users, only: [:show, :create, :update]
 end
