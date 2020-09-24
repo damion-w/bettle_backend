93f505ac167b5e1baa665e630e20cad68f10db71
Damion Wiltshire <drw.5452@gmail.com>
2020-09-24 10:05:08 -0700

    add update method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApiController
     before_action :require_login
 
     def show
-        event = Event.find(params[:id])
+        event = Event.find params[:id]
         render json: { event: event }
 
         # Leaving this here to remind myself that I have access to user data here
@@ -10,7 +10,7 @@ class EventsController < ApiController
     end
 
     def create
-        event = Event.new(event_params)
+        event = Event.new event_params
         event.user = current_user
         
         if event.save
@@ -20,8 +20,18 @@ class EventsController < ApiController
         end
     end
 
+    def update
+        event = Event.find params[:id] 
+
+        if event.update(event_params)
+            render json: { message: 'ok', event: event }
+        else
+            render json: { message: 'Could not update event' }
+        end
+    end
+
     def destroy
-        event = Event.find(params[:id])
+        event = Event.find params[:id]
         event.destroy
         head :ok
     end
