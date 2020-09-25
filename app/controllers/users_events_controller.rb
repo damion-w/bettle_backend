3927c25587444352f935a4844d5a0bdb3f7a29f2
Damion Wiltshire <drw.5452@gmail.com>
2020-09-25 10:20:08 -0700

    add update and destroy methods

--- app/controllers/users_events_controller.rb
+++ app/controllers/users_events_controller.rb
@@ -5,13 +5,36 @@ class UsersEventsController < ApiController
         event_id = params[:event_id]
         
         username = user_event_params["username"]
-
         user = User.find_by username: username
         user_id = user.id
 
         users_event = UsersEvent.create!({ user_id: user_id, event_id: event_id })
 
         render json: { message: 'ok', user_event: users_event }
+    end
+
+    def update
+        event_id = params[:event_id]
+        user_event = UsersEvent.find_by event_id: event_id
+        
+        username = user_event_params["username"]
+        user = User.find_by username: username
+        user_id = user.id
+
+        if user_event.update({ event_id: event_id, user_id: user_id })
+            render json: { message: 'ok', user_event: user_event }
+        else
+            render json: { message: 'Could not update user_event' }
+        end
+    end
+
+    def destroy
+        username = user_event_params["username"]
+        user = User.find_by username: username
+        user_id = user.id
+        user_event = UsersEvent.find_by user_id: user_id
+        user_event.destroy
+        head :ok
     end
 
     private
