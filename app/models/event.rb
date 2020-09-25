f03ca1aabe19e51ca5985079ea71c53ce816a18f
Damion Wiltshire <drw.5452@gmail.com>
2020-09-25 04:02:51 -0700

    add a method for returning expenses with events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ApplicationRecord
     belongs_to :user
     has_many :expenses
+
+    def to_json_with_expenses
+        expenses =  self.expenses 
+        {   id: self.id, 
+            name: self.name, 
+            date: self.date,
+            expenses: expenses 
+        }
+    end
 end
